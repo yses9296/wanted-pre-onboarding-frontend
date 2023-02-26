43cbb171cd312233c69fbec8498e4a2219f5b698
Eunseo Choi <yses9296@gmail.com>
2023-02-26 19:48:15 +0900

    fix: Validation 비밀번호 메세지 타입 수정

--- src/components/Validation.tsx
+++ src/components/Validation.tsx
@@ -8,7 +8,7 @@ type ValidationResponse = {
 
 function Validation({ email, password }: { email: string; password: string }): ValidationResponse {
 	const EMAIL_CHECK = new RegExp('[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.]+[a-zA-Z]+[.]*[a-zA-Z]*');
-	const PW_CHECK = new RegExp('(?=.{8,})');
+	const PW_CHECK = new RegExp('/.{8,}/');
 
 	const emailValid = EMAIL_CHECK.test(email);
 	const passwordValid = PW_CHECK.test(password);
@@ -17,7 +17,7 @@ function Validation({ email, password }: { email: string; password: string }): V
 		valid: Boolean(emailValid && passwordValid),
 		message: {
 			email: emailValid ? '' : '@를 포함한 이메일 형식이 아닙니다.',
-			password: emailValid ? '' : '8자 이상의 비밀번호가 아닙니다.',
+			password: passwordValid ? '' : '8자 이상의 비밀번호가 아닙니다.',
 		},
 	};
 }
