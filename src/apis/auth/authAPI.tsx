bead34e1d60d316b7e86ca1b39e760c88570a259
Eunseo Choi <yses9296@gmail.com>
2023-02-26 18:45:56 +0900

    fix: Auth API return data 수정

--- src/apis/auth/authAPI.tsx
+++ src/apis/auth/authAPI.tsx
@@ -4,7 +4,7 @@ import { Auth, SignUpResponseAuth, SignInResponseAuth } from 'apis/auth/authApi.
 
 export async function signup({ email, password }: Auth): Promise<SignUpResponseAuth> {
 	return new Promise((resolve, reject) => {
-		let message: string = '';
+		let errMsg: string = '';
 		signUpInstance({ email, password })
 			.then((res: AxiosResponse) => {
 				resolve({ message: '회원가입에 성공하였습니다. 로그인을 시도해주세요.' });
@@ -12,11 +12,11 @@ export async function signup({ email, password }: Auth): Promise<SignUpResponseA
 			.catch((error: AxiosError) => {
 				const status = error.response?.status;
 				if (status === 400) {
-					message = '이미 존재하는 이메일입니다.';
+					errMsg = '이미 존재하는 이메일입니다.';
 				} else {
-					message = '알 수 없는 오류가 발생했습니다. 다시 시도해 주세요.';
+					errMsg = '알 수 없는 오류가 발생했습니다. 다시 시도해 주세요.';
 				}
-				reject(message);
+				reject({ message: errMsg });
 			});
 	});
 }
@@ -29,15 +29,15 @@ export async function signin({ email, password }: Auth): Promise<SignInResponseA
 			})
 			.catch((error: AxiosError) => {
 				const status = error.response?.status;
-				let message: string = '';
+				let errMsg: string = '';
 				if (status === 401) {
-					message = '이메일 또는 비밀번호가 일치하지 않습니다.';
+					errMsg = '이메일 또는 비밀번호가 일치하지 않습니다.';
 				} else if (status === 404) {
-					message = '사용자를 찾을 수 없습니다. 다시 시도해 주세요.';
+					errMsg = '사용자를 찾을 수 없습니다. 다시 시도해 주세요.';
 				} else {
-					message = '알 수 없는 오류가 발생했습니다. 다시 시도해 주세요.';
+					errMsg = '알 수 없는 오류가 발생했습니다. 다시 시도해 주세요.';
 				}
-				reject(message);
+				reject({ message: errMsg });
 			});
 	});
 }
--- src/components/SignForm.tsx
+++ src/components/SignForm.tsx
@@ -1,9 +1,10 @@
 import { signin, signup } from 'apis/auth/authApi';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Validation from './Validation';
 
 const SignForm = ({ page }: { page: string }) => {
+	const navigate = useNavigate();
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [focusOn, setFocusOn] = useState({
@@ -23,8 +24,18 @@ const SignForm = ({ page }: { page: string }) => {
 	const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		page === 'signup'
-			? signup({ email, password }).then(() => console.log('signup'))
-			: signin({ email, password }).then(() => console.log('signin'));
+			? signup({ email, password })
+					.then(res => {
+						alert(res.message);
+						navigate('/signin');
+					})
+					.catch(err => alert(err.message))
+			: signin({ email, password })
+					.then(res => {
+						localStorage.setItem('ACCESS_TOKEN', res.accessToken);
+						navigate('/todo');
+					})
+					.catch(err => alert(err.message));
 	};
 	return (
 		<>
