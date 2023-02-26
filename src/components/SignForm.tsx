55282cbf61f76661f48f7d7e0bc63a39a6f78ab2
Eunseo Choi <yses9296@gmail.com>
2023-02-26 17:56:19 +0900

    feat: 이메일, 비밀번호 Validation 추가

--- src/components/SignForm.tsx
+++ src/components/SignForm.tsx
@@ -1,29 +1,80 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Validation from './Validation';
+
 const SignForm = ({ page }: { page: string }) => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [focusOn, setFocusOn] = useState({
+		email: false,
+		password: false,
+	});
+
+	const focusOnHandler = (e: React.ChangeEvent<HTMLInputElement>, value: boolean) => {
+		setFocusOn({
+			...focusOn,
+			[e.target.name]: value,
+		});
+	};
+
+	const { valid: isValidForm, message: validMsg } = Validation({ email, password });
+
+	const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+	};
 	return (
 		<>
-			<form>
+			<form onSubmit={onSubmitHandler}>
 				<p>
 					<label>이메일</label>
-					<input type="text" placeholder="@를 포함한 형식의 이메일을 입력해주세요." />
+					<input
+						type="email"
+						name="email"
+						data-testid="email-input"
+						placeholder="@를 포함한 형식의 이메일을 입력해주세요."
+						value={email}
+						onChange={e => setEmail(e.target.value)}
+						onFocus={e => {
+							focusOnHandler(e, true);
+						}}
+						onBlur={e => {
+							focusOnHandler(e, false);
+						}}
+					/>
 				</p>
+				{focusOn.email && validMsg?.email ? <strong>{validMsg.email}</strong> : null}
 
 				<p>
 					<label>비밀번호</label>
-					<input type="text" placeholder="8자 이상의 비밀번호를 입력해주세요" />
+					<input
+						type="password"
+						name="password"
+						data-testid="password-input"
+						placeholder="8자 이상의 비밀번호를 입력해주세요"
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+						onFocus={e => {
+							focusOnHandler(e, true);
+						}}
+						onBlur={e => {
+							focusOnHandler(e, false);
+						}}
+					/>
 				</p>
+				{focusOn.password && validMsg?.password ? <strong>{validMsg.password}</strong> : null}
+
+				<button disabled={!isValidForm}>{page === 'signup' ? '회원가입' : '로그인'}</button>
 			</form>
-			<p>
-				{page === 'signup' ? (
-					<p>
-						이미 회원이신가요? <Link to="/signin">로그인</Link>
-					</p>
-				) : (
-					<p>
-						아직 회원이 아니신가요? <Link to="/signup">회원가입</Link>
-					</p>
-				)}
-			</p>
+
+			{page === 'signup' ? (
+				<p>
+					이미 회원이신가요? <Link to="/signin">로그인</Link>
+				</p>
+			) : (
+				<p>
+					아직 회원이 아니신가요? <Link to="/signup">회원가입</Link>
+				</p>
+			)}
 		</>
 	);
 };
---
+++ src/components/Validation.tsx
@@ -0,0 +1,25 @@
+type ValidationResponse = {
+	valid: boolean;
+	message: {
+		email?: string;
+		password?: string;
+	};
+};
+
+function Validation({ email, password }: { email: string; password: string }): ValidationResponse {
+	const EMAIL_CHECK = new RegExp('[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.]+[a-zA-Z]+[.]*[a-zA-Z]*');
+	const PW_CHECK = new RegExp('(?=.{8,})');
+
+	const emailValid = EMAIL_CHECK.test(email);
+	const passwordValid = PW_CHECK.test(password);
+
+	return {
+		valid: Boolean(emailValid && passwordValid),
+		message: {
+			email: emailValid ? '' : '@를 포함한 이메일 형식이 아닙니다.',
+			password: emailValid ? '' : '8자 이상의 비밀번호가 아닙니다.',
+		},
+	};
+}
+
+export default Validation;
