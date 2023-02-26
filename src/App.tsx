2d4b5acc11e04bcb9e77df16efa5c52d8bc2007a
Eunseo Choi <yses9296@gmail.com>
2023-02-26 14:24:56 +0900

    feat: React Router 설치 & Route 페이지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import TodoPage from './pages/TodoPage';
 
 function App() {
 	return (
-		<div className="App">
+		<div>
 			<h1>Todo List</h1>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<SignInPage />} />
+					<Route path="/signin" element={<SignInPage />} />
+					<Route path="/signup" element={<SignUpPage />} />
+					<Route path="/todo" element={<TodoPage />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
