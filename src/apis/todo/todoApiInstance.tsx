14e864e356ab3a389f29f1efb198cc8b39ed259b
Eunseo Choi <yses9296@gmail.com>
2023-02-27 21:50:31 +0900

    fix: clientTodoAPI import 오류 수정

--- src/apis/todo/todoApiInstance.tsx
+++ src/apis/todo/todoApiInstance.tsx
@@ -1,4 +1,4 @@
-import { clientAuthAPI } from 'apis/clientAPI';
+import { clientTodoAPI } from 'apis/clientAPI';
 import {
 	Todo,
 	CreateTodoRequestProps,
@@ -7,14 +7,14 @@ import {
 } from 'apis/todo/todoApi.type';
 
 export const renderTodosInstance = async () => {
-	return await clientAuthAPI.get<Todo[]>(`todos`);
+	return await clientTodoAPI.get<Todo[]>('todos');
 };
 export const createTodoInstance = async ({ todo }: CreateTodoRequestProps) => {
-	return await clientAuthAPI.post<Todo>(`todos`, { todo });
+	return await clientTodoAPI.post<Todo>(`todos`, { todo });
 };
 export const updateTodoInstance = async ({ id, todo, isCompleted }: UpdateTodoRequestProps) => {
-	return await clientAuthAPI.put<Todo>(`todos/${id}`, { todo, isCompleted });
+	return await clientTodoAPI.put<Todo>(`todos/${id}`, { todo, isCompleted });
 };
 export const deleteTodoInstance = async ({ id }: DeleteTodoRequestProps) => {
-	return await clientAuthAPI.delete(`todos/${id}`);
+	return await clientTodoAPI.delete(`todos/${id}`);
 };
